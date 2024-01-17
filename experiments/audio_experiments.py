ae47197cd51268848e1edd91b8c0631b7d42a779
Bailey Chittle <bachittle@gmail.com>
2024-01-16 23:33:04 -0500

    some code cleanup, prep matplotlib

--- experiments/audio_experiments.py
+++ experiments/audio_experiments.py
@@ -2,29 +2,16 @@ import open_voice_pilot.audio_processing as ap
 import time
 import argparse
 
-def print_audio_file_chunks(audio_file_path):
-    file_processor = ap.FileAudioProcessor(audio_file_path)
+def print_chunks(processor):
     i = 0
     start_time = time.time()
     while True:
-        chunk = file_processor.read_chunk()
+        chunk = processor.read_chunk()
         if not chunk:
             break
-        print(f"Chunk {i}: {len(chunk)}")
-        i += 1
-    
-    elapsed_time = time.time() - start_time
-    print(f"Elapsed time: {elapsed_time*1000:.2f} ms, average time per chunk: {(elapsed_time / i) * 1000:.2f} ms")
-
-def print_microphone_chunks():
-    mic_processor = ap.MicrophoneAudioProcessor()
-    i = 0
-    start_time = time.time()
-    print("Press ctrl + c to stop the program\n")
-    while True:
-        chunk = mic_processor.read_chunk()
         elapsed_time = time.time() - start_time
-        print(f"\rChunk {i}: {len(chunk)}, elapsed time: {elapsed_time:.2f} s, average time per chunk: {(elapsed_time / (i + 1)) * 1000:.2f} ms", end="")
+        avg_time_per_chunk = (elapsed_time / i) * 1000 if i != 0 else 0
+        print(f"\rChunk {i}: {len(chunk)}, Elapsed time: {elapsed_time:.2f} s, average time per chunk: {avg_time_per_chunk:.2f} ms.  ", end="")
         i += 1
 
 def main():
@@ -38,9 +25,14 @@ def main():
         if not args.file:
             print("Audio file path is required for 'file' mode.")
             return
-        print_audio_file_chunks(args.file)
+        processor = ap.FileAudioProcessor(args.file)
     elif args.mode == 'mic':
-        print_microphone_chunks()
+        processor = ap.MicrophoneAudioProcessor()
+
+    print_chunks(processor)
 
 if __name__ == "__main__":
     main()
+
+
+
